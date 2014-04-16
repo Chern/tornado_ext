b2c30c53726c19f07917c098b73dbb42a813c57a
Alexander Chernigov <aleksandr.chernigov@gmail.com>
2014-04-16 16:17:49 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'tornado_ext',
+    version = '0.1',
+    author = 'Alexander Chernigov',
+    author_email = 'aleksandr.chernigov@gmail.com',
+    url = 'https://github.com/Chern/tornado_ext',
+    packages = ['tornado_ext']   
+)
